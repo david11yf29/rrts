ca1e4af24d46605d1b6b54427284422d2972b284
david11yf29 <david11yf29@gmail.com>
2021-01-14 21:27:32 +0800

    updated

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,9 +9,22 @@ interface AppProps {
 }
 
 class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList = (): JSX.Element[] => {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>
+    })
+  }
+
   render() {
     return (
-      <div>Hi there!</div>
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
     )
   }
 }
